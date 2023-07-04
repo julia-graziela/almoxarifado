bcb497cbd99697f853ced7bc9be1a5008b2ff5e0
julia-graziela <72137213+julia-graziela@users.noreply.github.com>
2023-07-04 14:30:18 -0300

    Sidebar com itens

--- estoque/urls.py
+++ estoque/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'estoque'
 
 urlpatterns = [
-    path('', views.lista_produtos, name='lista_produtos'),
+    path('estoque/', views.lista_produtos, name='lista_produtos'),
     path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
     path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
     path('remover/<int:produto_id>/', views.remover_produto, name='remover_produto'),
